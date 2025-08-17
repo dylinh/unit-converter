83c4aa050044a6dd58e8c6b2fafd38503cb2f14c
Dylinh Bun <dylinhb@gmail.com>
2025-08-17 16:44:06 -0400

    Update index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+// Elements requiring JS
+const inputBoxEl = document.getElementById("unit-input")
+const convertButtonEl = document.getElementById("convert-btn")
+const lengthEl = document.getElementById("length")
+const volumeEl = document.getElementById("volume")
+const massEl = document.getElementById("mass")
+
+// Create an event listener for the convert button
+convertButtonEl.addEventListener("click", function(){
+    console.log("Test")
+})
+// Use the value in the input box to output the conversions
+
